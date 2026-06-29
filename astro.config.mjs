6296dbb91fee088f1a49e12a6bf0fd0a2fd7261a
Ronan Berder <hunvreus@gmail.com>
2026-06-29 12:57:50 +0800

    Prepare 1.0.8 release

--- astro.config.mjs
+++ astro.config.mjs
@@ -24,7 +24,7 @@ export default defineConfig({
       },
       site: {
         title: "ReallySimpleDocs",
-        subtitle: "v1.0.7",
+        subtitle: "v1.0.8",
         description: "A really simple documentation system for Astro.",
         url: "https://reallysimpledocs.com",
         favicon: "favicon.svg",
